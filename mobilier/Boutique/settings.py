7c1be8fca1a4aaf48f892c4fb35d5142cd8d4ae0
kevine659 <kevinemafouomenekenne@gmail.com>
2026-07-23 09:18:18 +0100

    server media en production

--- mobilier/Boutique/settings.py
+++ mobilier/Boutique/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+yx^v)^g-dkp_wieimwo7hrr8^r9tn!-$1c4us_8*4_jge8x$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS=[
     "newbackendimmob-production.up.railway.app",
